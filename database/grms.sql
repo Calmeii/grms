02ecf995104d5d82b6cfe54206612d6ee1694e88
Calmeii <hk_huang_kun@163.com>
2015-06-19 17:34:33 +0800

    Revert "add数据库"
    This reverts commit bf3e01b50836aef44ec59f795a0695de0c7a3599.

--- database/grms.sql
+++ database/grms.sql
@@ -1,189 +1,283 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
 --
--- 主机: 127.0.0.1
--- 生成日期: 2015 �?06 �?19 �?11:05
--- 服务器版本: 5.6.11
--- PHP 版本: 5.5.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: grms
+-- ------------------------------------------------------
+-- Server version	5.6.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- 数据库: `grms`
---
-CREATE DATABASE IF NOT EXISTS `grms` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `grms`;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `branchs`
+-- Table structure for table `branch_right`
 --
 
-CREATE TABLE IF NOT EXISTS `branchs` (
+DROP TABLE IF EXISTS `branch_right`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `branch_right` (
+  `branch_id` int(11) DEFAULT NULL,
+  `right_id` int(11) DEFAULT NULL,
+  KEY `branch_id` (`branch_id`),
+  KEY `right_id` (`right_id`),
+  CONSTRAINT `branch_right_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branchs` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `branch_right_ibfk_2` FOREIGN KEY (`right_id`) REFERENCES `rights` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branch_right`
+--
+
+LOCK TABLES `branch_right` WRITE;
+/*!40000 ALTER TABLE `branch_right` DISABLE KEYS */;
+/*!40000 ALTER TABLE `branch_right` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `branch_role`
+--
+
+DROP TABLE IF EXISTS `branch_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `branch_role` (
+  `branch_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  KEY `branch_id` (`branch_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `branch_role_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branchs` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `branch_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branch_role`
+--
+
+LOCK TABLES `branch_role` WRITE;
+/*!40000 ALTER TABLE `branch_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `branch_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `branch_user`
+--
+
+DROP TABLE IF EXISTS `branch_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `branch_user` (
+  `branch_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  KEY `branch_id` (`branch_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `branch_user_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branchs` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `branch_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branch_user`
+--
+
+LOCK TABLES `branch_user` WRITE;
+/*!40000 ALTER TABLE `branch_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `branch_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `branchs`
+--
+
+DROP TABLE IF EXISTS `branchs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `branchs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `lst_id` int(11) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `lst_id` (`lst_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `branch_right`
---
-
-CREATE TABLE IF NOT EXISTS `branch_right` (
-  `branch_id` int(11) DEFAULT NULL,
-  `right_id` int(11) DEFAULT NULL,
-  KEY `branch_id` (`branch_id`),
-  KEY `right_id` (`right_id`)
+  KEY `lst_id` (`lst_id`),
+  CONSTRAINT `branchs_ibfk_1` FOREIGN KEY (`lst_id`) REFERENCES `branchs` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `branch_role`
---
-
-CREATE TABLE IF NOT EXISTS `branch_role` (
-  `branch_id` int(11) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL,
-  KEY `branch_id` (`branch_id`),
-  KEY `role_id` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `branch_user`
+-- Dumping data for table `branchs`
 --
 
-CREATE TABLE IF NOT EXISTS `branch_user` (
-  `branch_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  KEY `branch_id` (`branch_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+LOCK TABLES `branchs` WRITE;
+/*!40000 ALTER TABLE `branchs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `branchs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- 表的结构 `logs`
+-- Table structure for table `logs`
 --
 
-CREATE TABLE IF NOT EXISTS `logs` (
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   `descp` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `rights`
+-- Dumping data for table `logs`
 --
 
-CREATE TABLE IF NOT EXISTS `rights` (
+LOCK TABLES `logs` WRITE;
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rights`
+--
+
+DROP TABLE IF EXISTS `rights`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rights` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 转存表中的数据 `rights`
+-- Dumping data for table `rights`
 --
 
-INSERT INTO `rights` (`id`, `name`) VALUES
-(1, '权限一'),
-(2, '权限二'),
-(3, '权限一'),
-(4, '权限二'),
-(5, '权限三'),
-(6, '权限四'),
-(7, '权限五'),
-(8, '权限六                                               ');
-
--- --------------------------------------------------------
+LOCK TABLES `rights` WRITE;
+/*!40000 ALTER TABLE `rights` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rights` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- 表的结构 `roles`
+-- Table structure for table `role_right`
 --
 
-CREATE TABLE IF NOT EXISTS `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
-
---
--- 转存表中的数据 `roles`
---
-
-INSERT INTO `roles` (`id`, `name`) VALUES
-(1, '角色一'),
-(2, '角色二'),
-(3, '角色三'),
-(4, '角色四'),
-(5, '角色五'),
-(6, '角色六');
-
--- --------------------------------------------------------
-
---
--- 表的结构 `role_right`
---
-
-CREATE TABLE IF NOT EXISTS `role_right` (
+DROP TABLE IF EXISTS `role_right`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_right` (
   `role_id` int(11) DEFAULT NULL,
   `right_id` int(11) DEFAULT NULL,
   KEY `right_id` (`right_id`),
-  KEY `role_id` (`role_id`)
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `role_right_ibfk_1` FOREIGN KEY (`right_id`) REFERENCES `rights` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `role_right_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `role_user`
+-- Dumping data for table `role_right`
 --
 
-CREATE TABLE IF NOT EXISTS `role_user` (
+LOCK TABLES `role_right` WRITE;
+/*!40000 ALTER TABLE `role_right` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role_right` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_user`
+--
+
+DROP TABLE IF EXISTS `role_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_user` (
   `role_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   KEY `user_id` (`user_id`),
-  KEY `role_id` (`role_id`)
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `role_user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `role_user_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 转存表中的数据 `role_user`
+-- Dumping data for table `role_user`
 --
 
-INSERT INTO `role_user` (`role_id`, `user_id`) VALUES
-(2, 1),
-(2, 2),
-(2, 3),
-(3, 1),
-(3, 3);
-
--- --------------------------------------------------------
+LOCK TABLES `role_user` WRITE;
+/*!40000 ALTER TABLE `role_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role_user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- 表的结构 `users`
+-- Table structure for table `roles`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_right`
+--
+
+DROP TABLE IF EXISTS `user_right`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_right` (
+  `user_id` int(11) DEFAULT NULL,
+  `right_id` int(11) DEFAULT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `right_id` (`right_id`),
+  CONSTRAINT `user_right_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `user_right_ibfk_2` FOREIGN KEY (`right_id`) REFERENCES `rights` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_right`
+--
+
+LOCK TABLES `user_right` WRITE;
+/*!40000 ALTER TABLE `user_right` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_right` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) DEFAULT NULL,
   `sex` varchar(6) DEFAULT NULL,
@@ -191,109 +285,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   `tel` varchar(20) DEFAULT NULL,
   `password` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 转存表中的数据 `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `sex`, `email`, `tel`, `password`) VALUES
-(1, 'admin', '男', 'xxx@xx.com', '10086', '123'),
-(2, '用户一', '男', '589@qq.com', '10011', 'nebg'),
-(3, '用户二', '男', '5XX@qq.com', '114', 'nebg'),
-(4, '用户三', '女', '1239@qq.com', '154', 'nbgd'),
-(5, '用户四', '男', '5X9@qq.com', '114', 'nebg'),
-(6, '用户五', '男', '58921@qq.com', '114', 'nebg'),
-(7, '用户六', '男', '58XX9@qq.com', '114', 'nebg');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'admin','男','xxx@xx.com','10086','123');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `user_right`
---
-
-CREATE TABLE IF NOT EXISTS `user_right` (
-  `user_id` int(11) DEFAULT NULL,
-  `right_id` int(11) DEFAULT NULL,
-  KEY `user_id` (`user_id`),
-  KEY `right_id` (`right_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 转存表中的数据 `user_right`
---
-
-INSERT INTO `user_right` (`user_id`, `right_id`) VALUES
-(1, 1),
-(1, 1),
-(1, 2),
-(1, 3),
-(1, 4),
-(1, 5),
-(2, 1),
-(2, 3),
-(3, 1),
-(3, 3),
-(3, 4);
-
---
--- 限制导出的表
---
-
---
--- 限制表 `branchs`
---
-ALTER TABLE `branchs`
-  ADD CONSTRAINT `branchs_ibfk_1` FOREIGN KEY (`lst_id`) REFERENCES `branchs` (`id`) ON DELETE CASCADE;
-
---
--- 限制表 `branch_right`
---
-ALTER TABLE `branch_right`
-  ADD CONSTRAINT `branch_right_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branchs` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `branch_right_ibfk_2` FOREIGN KEY (`right_id`) REFERENCES `rights` (`id`) ON DELETE CASCADE;
-
---
--- 限制表 `branch_role`
---
-ALTER TABLE `branch_role`
-  ADD CONSTRAINT `branch_role_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branchs` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `branch_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
-
---
--- 限制表 `branch_user`
---
-ALTER TABLE `branch_user`
-  ADD CONSTRAINT `branch_user_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branchs` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `branch_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
--- 限制表 `logs`
---
-ALTER TABLE `logs`
-  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
--- 限制表 `role_right`
---
-ALTER TABLE `role_right`
-  ADD CONSTRAINT `role_right_ibfk_1` FOREIGN KEY (`right_id`) REFERENCES `rights` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `role_right_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
-
---
--- 限制表 `role_user`
---
-ALTER TABLE `role_user`
-  ADD CONSTRAINT `role_user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `role_user_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
-
---
--- 限制表 `user_right`
---
-ALTER TABLE `user_right`
-  ADD CONSTRAINT `user_right_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `user_right_ibfk_2` FOREIGN KEY (`right_id`) REFERENCES `rights` (`id`) ON DELETE CASCADE;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-06-02 21:16:07
