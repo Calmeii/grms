b7ee4b2d412ccb70c7c481d4979de0fe76c8406f
Calmeii <hk_huang_kun@163.com>
2015-06-24 18:29:27 +0800

    用户管理更新

--- coder/user_info.php
+++ coder/user_info.php
@@ -1,1 +1,76 @@
-﻿
+<?php
+session_start();
+if (isset($_SESSION['USERNAME']) == FALSE || $_SESSION['USERNAME'] != 'admin') {
+	header("Location: ".$config_basedir);
+}
+require("../config.php");
+$db = mysql_connect($dbhost, $dbuser, $dbpassword);
+mysql_select_db($dbdatabase, $db);
+if (isset($_POST['modify_user_info'])) {
+	$sql1 = "update users set username='".$_POST['username']."',sex='".$_POST['sex']."',
+	tel='".$_POST['tel']."',email='".$_POST['email']."' where id=".$_GET['id'].";";
+	mysql_query($sql1);
+}
+require("header.php");
+?>
+
+<?php
+echo "<div id='modify_user_info'>";
+	$user_sql = "select * from users where id=".$_GET['id'].";";
+	$user_res = mysql_query($user_sql);
+	$user_row = mysql_fetch_assoc($user_res);
+	echo "<form action='user_info.php?id=".$_GET['id']."' method='post'>";
+	echo "姓名: <input type='text' name='username' value='".$user_row['username']."'><br />";
+	echo "性别: <input type='text' name='sex' value='".$user_row['sex']."'><br />";
+	echo "电话: <input type='text' name='tel' value='".$user_row['tel']."'><br />";
+	echo "邮箱: <input type='text' name='email' value='".$user_row['email']."'><br />";
+	echo "<input type='submit' name='modify_user_info' value='修改'>";
+	echo "</form>";
+echo "</div>";
+echo "<div id='user_char'>";
+	$char_sql = "select * from roles where id in(select role_id from
+				role_user where user_id=".$_GET['id'].");";
+	$char_res = mysql_query($char_sql);
+	echo "<table><tr><th>用户角色</th></tr>";
+	while ($char_row = mysql_fetch_assoc($char_res))
+	{
+		echo "<tr>";
+		echo "<td>".$char_row['name']."</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
+echo "</div>";
+?>
+
+<?php 
+require("footer.php");
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- coder/user_manage.php
+++ coder/user_manage.php
@@ -1,22 +1,15 @@
 <?php
+session_start();
 if (isset($_SESSION['USERNAME']) == FALSE || $_SESSION['USERNAME'] != 'admin') {
 	header("Location: ".$config_basedir);
 }
-//session_start();
 require("../config.php");
 $db = mysql_connect($dbhost, $dbuser, $dbpassword);
 mysql_select_db($dbdatabase, $db);
 
-if (isset($_POST['edit'])) {
-	$sql = "update users set username='".$_POST['username']."', 
-	sex='".$_POST['sex']."', email='".$_POST['email']."', tel='".$_POST['tel'].
-	"' where id=".$_GET['id'].";";
-	header("Location:".$_SERVER['PHP_SELF']);
-}
-else if (isset($_POST['delete'])) {
+if (isset($_POST['delete'])) {
 	$sql = "delete from users where id=".$_GET['id'].";";
 	mysql_query($sql);
-	header("Location:".$_SERVER['PHP_SELF']);
 }
 else if (isset($_POST['add'])) {
 	$sql = "insert into users(username,sex,tel,email) values('"
@@ -31,13 +24,16 @@ require("header.php");
 <?php
 $sql = "select * from users;";
 $res = mysql_query($sql);
-echo "<table class='now'>";
-echo "<tr><th>姓名</th><th>性别</th><th>电话</th><th>邮箱</th><th>编辑</th></tr>";
+
+echo "<table class='now' style='width:600'>";
+echo "<tr><th>姓名</th><th>性别</th><th>电话</th>
+	<th>邮箱</th><th>编辑</th></tr>";
 while ($row = mysql_fetch_assoc($res))
 {
-	echo "<form action=".$config_basedir."/coder/user_manage.php?id=".$row['id']." method='post'>";
+	echo "<form action=".$config_basedir.
+		"/coder/user_manage.php?id=".$row['id']." method='post'>";
 	echo "<tr>";
-	echo "<td><a href='user_info.php?id=".$row['id']."'>".$row['username']."</td>";
+	echo "<td><a href='user_info.php?id=".$row['id']."'>".$row['username']."</a></td>";
 	echo "<td>".$row['sex']."</td>";
 	echo "<td>".$row['tel']."</td>";
 	echo "<td>".$row['email']."</td>";
@@ -51,7 +47,6 @@ echo '<tr>
 	<td><input type="text" name="sex"> </td>
 	<td><input type="text" name="tel"> </td>
 	<td><input type="text" name="email"> </td>
-	<td> </td>
 	<td><input type="submit" name="add" value="增加"> </td>
 </tr>';
 echo '</form>';
